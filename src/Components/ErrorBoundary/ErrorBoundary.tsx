2eeac11547244f5eaf5b97ef14fcd032394afb58
Anar Agaev <anar.n.agaev@gmail.com>
2024-04-25 13:15:42 +0300

    Added Error Boundary

---
+++ src/Components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import style from './ErrorBoundary.module.sass'
+import image from './error.webp'
+
+interface Props {
+    children: React.ReactNode
+}
+
+interface ErrorState {
+    hasError: boolean
+    error?: Error
+    errorInfo?: React.ErrorInfo
+}
+
+const { container, title, subtitle, picture } = style
+
+class ErrorBoundary extends React.Component<Props, ErrorState> {
+    constructor(props: Props) {
+        super(props);
+        this.state = { hasError: false }
+    }
+
+    componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+        this.setState({ hasError: true, error, errorInfo })
+    }
+
+    render() {
+        const { hasError, error, errorInfo } = this.state
+
+        if (hasError) {
+            console.error(error?.toString())
+            console.error(errorInfo?.componentStack)
+
+            return (
+                <div className={container}>
+                    <h1 className={title}>Что-то пошло не так!</h1>
+                    <p className={subtitle}>Попробуйте перезагрузить страницу или зайти на сайт позже.</p>
+                    <img className={picture} src={image} alt="Что-то пошло не так."/>
+                </div>
+            );
+        }
+
+        return this.props.children
+    }
+}
+
+export default ErrorBoundary
